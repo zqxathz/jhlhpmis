963894b553c6a8f2a8ce78de5a596163a44d2ae1
zqxathz <zqxathz@qq.com>
2017-07-05 17:59:47 +0800

    2017-7-5 17:59:43

--- server/ServerMethodsUnit.dfm
+++ server/ServerMethodsUnit.dfm
@@ -9,13 +9,13 @@ object ServerMethods: TServerMethods
       'Database=jhlh'
       'CharacterSet=utf8'
       'DriverID=MySQL')
-    Left = 328
-    Top = 160
+    Left = 56
+    Top = 32
   end
   object FDPhysMySQLDriverLink1: TFDPhysMySQLDriverLink
     VendorLib = '/lib64/mysql/libmysqlclient.so'
-    Left = 120
-    Top = 176
+    Left = 128
+    Top = 32
   end
   object expoFDQuery: TFDQuery
     CachedUpdates = True
@@ -25,12 +25,12 @@ object ServerMethods: TServerMethods
       
         'select * from jhlh_pmis_expo where status=1 and trash=0 and clie' +
         'ntvisable=1')
-    Left = 328
-    Top = 256
+    Left = 248
+    Top = 248
   end
   object FDStanStorageBinLink1: TFDStanStorageBinLink
-    Left = 200
-    Top = 176
+    Left = 208
+    Top = 32
   end
   object customertypeFDQuery: TFDQuery
     Connection = FDConnection1
@@ -46,7 +46,34 @@ object ServerMethods: TServerMethods
     ResourceOptions.StoreMergeMeta = mmAdd
     SQL.Strings = (
       'select * from jhlh_pmis_paytype where status=1 and trash=0')
-    Left = 208
-    Top = 240
+    Left = 168
+    Top = 248
+  end
+  object expotypeFDQuery: TFDQuery
+    Connection = FDConnection1
+    ResourceOptions.AssignedValues = [rvStoreMergeData, rvStoreMergeMeta]
+    ResourceOptions.StoreMergeData = dmDataAppend
+    ResourceOptions.StoreMergeMeta = mmAdd
+    SQL.Strings = (
+      'select * from jhlh_pmis_expotype where status=1 and trash=0')
+    Left = 80
+    Top = 248
+  end
+  object shoppersourceFDQuery: TFDQuery
+    Connection = FDConnection1
+    ResourceOptions.AssignedValues = [rvStoreMergeData, rvStoreMergeMeta]
+    ResourceOptions.StoreMergeData = dmDataAppend
+    ResourceOptions.StoreMergeMeta = mmAdd
+    SQL.Strings = (
+      'select * from jhlh_pmis_shopper_sourcetype where status=1')
+    Left = 336
+    Top = 248
+  end
+  object customerFDQuery: TFDQuery
+    OnUpdateError = customerFDQueryUpdateError
+    Connection = FDConnection1
+    UpdateOptions.UpdateTableName = 'jhlh_pmis_customers'
+    Left = 64
+    Top = 128
   end
 end
--- server/ServerMethodsUnit.pas
+++ server/ServerMethodsUnit.pas
@@ -3,15 +3,17 @@ unit ServerMethodsUnit;
 interface
 
 uses System.SysUtils, System.Classes, System.Json,
-    DataSnap.DSProviderDataModuleAdapter,
-    Datasnap.DSServer, Datasnap.DSAuth, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  DataSnap.DSProviderDataModuleAdapter,
+  DataSnap.DSServer, DataSnap.DSAuth, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
   FireDAC.Phys.MySQLDef, FireDAC.ConsoleUI.Wait, Data.DB, FireDAC.Comp.Client,
   FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
-  FireDAC.Stan.StorageBin, FireDAC.Comp.DataSet;
+  FireDAC.Stan.StorageBin, FireDAC.Comp.DataSet,System.Generics.Collections,System.Variants;
 
 type
+  TErrorRecordIDs = array of integer;
+
   TServerMethods = class(TDSServerModule)
     FDConnection1: TFDConnection;
     FDPhysMySQLDriverLink1: TFDPhysMySQLDriverLink;
@@ -19,41 +21,155 @@ type
     FDStanStorageBinLink1: TFDStanStorageBinLink;
     customertypeFDQuery: TFDQuery;
     paytypeFDQuery: TFDQuery;
-  private
-    { Private declarations }
-  public
-    { Public declarations }
-    function EchoString(Value: string): string;
-    function ReverseString(Value: string): string;
-    function ExpoData(username:string;password:string):TStream;
-    function CustomertypeData(username:string;password:string):TStream;
-    function PaytypeData(username:string;password:string):TStream;
-  end;
+    expotypeFDQuery: TFDQuery;
+    shoppersourceFDQuery: TFDQuery;
+    customerFDQuery: TFDQuery;
+    procedure customerFDQueryUpdateError(ASender: TDataSet; AException: EFDException;
+      ARow: TFDDatSRow; ARequest: TFDUpdateRequest; var AAction: TFDErrorAction);
+    private
+      { Private declarations }
+      FUpdateErrorRecords:TErrorRecordIDs;
+      FErrorsList: TJSONObject;
+      function GenerateErrorMessage: string;
+    public
+      { Public declarations }
+      function EchoString(Value: string): string;
+      function ReverseString(Value: string): string;
+      function ExpoData(username: string; password: string): TStream;
+      function CustomertypeData(username: string; password: string): TStream;
+      function PaytypeData(username: string; password: string): TStream;
+      function ExpoTypeData(username: string; password: string): TStream;
+      function ShopperSourceData(username: string; password: string): TStream;
+      function CustomerDataPost(AStream: TStream):string;
+    end;
 
 implementation
 
 {%CLASSGROUP 'System.Classes.TPersistent'}
-
-
 {$R *.dfm}
 
+uses System.StrUtils,System.DateUtils;
 
-uses System.StrUtils;
+function CopyStream(const AStream: TStream): TMemoryStream;
+var
+  LBuffer: TBytes;
+  LCount: Integer;
+begin
+  Result := TMemoryStream.Create;
+  try
+    SetLength(LBuffer, 1024 * 32);
+    while True do
+    begin
+      LCount := AStream.Read(LBuffer, Length(LBuffer));
+      Result.Write(LBuffer, LCount);
+      if LCount < Length(LBuffer) then
+        break;
+    end;
+  except
+    Result.Free;
+    raise;
+  end;
+end;
+
+function TServerMethods.CustomerDataPost(AStream: TStream):string;
+var
+  LMemStream: TMemoryStream;
+  LErrors: Integer;
+  count:int64;
+  starttime,endtime:TDateTime;
+begin
+  FErrorsList := TJSONObject.Create;
+  LMemStream:=CopyStream(AStream);
+  LMemStream.Position:=0;
+  try
+    customerFDQuery.Close;
+    customerFDQuery.LoadFromStream(LMemStream,TFDStorageFormat.sfBinary);
+    Writeln('start');
+    starttime:=now();
+    FDConnection1.StartTransaction;
+
+    LErrors:=customerFDQuery.ApplyUpdates;
+    FDConnection1.Commit;
+    {
+    if LErrors<>0 then
+      FDConnection1.Rollback
+    else
+      FDConnection1.Commit;
+     }
+    endtime:=now();
+    count:=MilliSecondsBetween(endtime,starttime);
+    Writeln(inttostr(count));
+  finally
+    customerFDQuery.Close;
+    LMemStream.Free;
+    FDConnection1.Close;
+    Writeln('ok');
+    if LErrors>0 then
+    begin
+      //Result := Format(sErrorsOnApplyUpdates, [GenerateErrorMessage]);
+      //Writeln(Result);
+      Result:=FErrorsList.ToJSON;
+    end;
+    FErrorsList.Free;
+  end;
+end;
+
+procedure TServerMethods.customerFDQueryUpdateError(ASender: TDataSet; AException: EFDException;
+  ARow: TFDDatSRow; ARequest: TFDUpdateRequest; var AAction: TFDErrorAction);
+var
+  LDataStr: string;
+begin
+  try
+    try
+      if not VarIsNull(ARow.GetData('id')) then LDataStr := ARow.GetData('id');
+      FErrorsList.AddPair('Errorid', LDataStr);
+    except
+
+    end;
+  finally
+
+  end;
+end;
+
+function TServerMethods.GenerateErrorMessage: string;
+var
+  I: Integer;
+  LJSONObject: TJSONObject;
+  LJSONArray: TJSONArray;
+begin
+  LJSONObject := TJSONObject.Create;
+  try
+    LJSONArray := TJSONArray.Create;
+    try
+      for I := FErrorsList.Count - 1 downto 0 do
+        LJSONArray.Add(FErrorsList[I]);
+
+      LJSONObject.AddPair('Errors', LJSONArray);
+      Result := LJSONObject.ToJSON;
+    except
+      LJSONArray.Free;
+    end;
+  except
+    LJSONObject.Free
+  end;
+  FErrorsList.Clear;
+end;
 
 function TServerMethods.CustomertypeData(username, password: string): TStream;
 begin
-  Result:=TMemoryStream.Create;
+  Result := TMemoryStream.Create;
   try
     try
       FDConnection1.Open;
       customertypeFDQuery.Close;
       customertypeFDQuery.Open;
-      customertypeFDQuery.SaveToStream(Result,TFDStorageFormat.sfBinary);
-      Result.Position:=0;
+      customertypeFDQuery.SaveToStream(Result, TFDStorageFormat.sfBinary);
+      Result.Position := 0;
     except
       on E: Exception do
       begin
         Writeln(E.Message);
+        Result.Free;
       end;
     end;
   finally
@@ -69,18 +185,19 @@ end;
 
 function TServerMethods.ExpoData(username, password: string): TStream;
 begin
-  Result:=TMemoryStream.Create;
+  Result := TMemoryStream.Create;
   try
     try
       FDConnection1.Open;
       expoFDQuery.Close;
       expoFDQuery.Open;
-      expoFDQuery.SaveToStream(Result,TFDStorageFormat.sfBinary);
-      Result.Position:=0;
+      expoFDQuery.SaveToStream(Result, TFDStorageFormat.sfBinary);
+      Result.Position := 0;
     except
       on E: Exception do
       begin
         Writeln(E.Message);
+        Result.Free;
       end;
     end;
   finally
@@ -90,20 +207,44 @@ begin
 
 end;
 
+function TServerMethods.ExpoTypeData(username, password: string): TStream;
+begin
+  Result := TMemoryStream.Create;
+  try
+    try
+      FDConnection1.Open;
+      expotypeFDQuery.Close;
+      expotypeFDQuery.Open;
+      expotypeFDQuery.SaveToStream(Result, TFDStorageFormat.sfBinary);
+      Result.Position := 0;
+    except
+      on E: Exception do
+      begin
+        Writeln(E.Message);
+        Result.Free;
+      end;
+    end;
+  finally
+    expotypeFDQuery.Close;
+    FDConnection1.Close;
+  end;
+end;
+
 function TServerMethods.PaytypeData(username, password: string): TStream;
 begin
-  Result:=TMemoryStream.Create;
+  Result := TMemoryStream.Create;
   try
     try
       FDConnection1.Open;
       paytypeFDQuery.Close;
       paytypeFDQuery.Open;
-      paytypeFDQuery.SaveToStream(Result,TFDStorageFormat.sfBinary);
-      Result.Position:=0;
+      paytypeFDQuery.SaveToStream(Result, TFDStorageFormat.sfBinary);
+      Result.Position := 0;
     except
       on E: Exception do
       begin
         Writeln(E.Message);
+        Result.Free;
       end;
     end;
   finally
@@ -112,10 +253,33 @@ begin
   end;
 end;
 
+function TServerMethods.ShopperSourceData(username, password: string): TStream;
+begin
+  Result := TMemoryStream.Create;
+  try
+    try
+      FDConnection1.Open;
+      shoppersourceFDQuery.Close;
+      shoppersourceFDQuery.Open;
+      shoppersourceFDQuery.SaveToStream(Result, TFDStorageFormat.sfBinary);
+      Result.Position := 0;
+    except
+      on E: Exception do
+      begin
+        Writeln(E.Message);
+        Result.Free;
+      end;
+    end;
+  finally
+    shoppersourceFDQuery.Close;
+    FDConnection1.Close;
+  end;
+
+end;
+
 function TServerMethods.ReverseString(Value: string): string;
 begin
   Result := System.StrUtils.ReverseString(Value);
 end;
 
 end.
-
